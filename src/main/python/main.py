c56dbd5cfdaa52e63bc0f002b7cbc7d4743eb4ee
mylanng <162841372+mylanng@users.noreply.github.com>
2024-07-14 14:07:43 +0000

    added python file

---
+++ src/main/python/main.py
@@ -0,0 +1,21 @@
+import math
+
+def countEvenNum (*arr, size):
+  count = 0
+  for x in size:
+    if (arr[x]%2==0):
+      count += 1
+
+  return count
+
+n = 5
+foo [5] = {0,1,2,3,4}
+
+m = 100
+bar[100]
+
+for i in m:
+  bar[i] = i
+
+print("Number of even numbers in foo: ", countEvenNum(foo, n))
+#print("Number of even numbers in bar: ", countEvenNum(bar, m))
