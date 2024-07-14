630aa980c1d669c036afc020a27374b84211cc42
mylanng <162841372+mylanng@users.noreply.github.com>
2024-07-14 14:23:01 +0000

    fixed syntax errors

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,20 +2,20 @@ import math
 
 def countEvenNum (*arr, size):
   count = 0
-  for x in size:
+  for x in range (size):
     if (arr[x]%2==0):
       count += 1
 
   return count
 
 n = 5
-foo [5] = {0,1,2,3,4}
+foo = {0,1,2,3,4}
 
 m = 100
-bar[100]
+bar = {}
 
-for i in m:
+for i in range (m):
   bar[i] = i
 
-print("Number of even numbers in foo: ", countEvenNum(foo, n))
+print("Number of even numbers in foo: " , countEvenNum(foo, n))
 #print("Number of even numbers in bar: ", countEvenNum(bar, m))
